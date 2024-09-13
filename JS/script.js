ad97b69c3efa300b8b546b37c3190a87721764f3
vedlearns <Vedankxb@gmail.com>
2024-09-13 16:50:07 +0530

    v8 Complete

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,9 @@
-let username = "vedank"
+debugger 
+console.log(username);
+var  username = "vedank"
 let age = 25
 let isHappy = true
 let userIntro ="hello i am "+ username + "singh "
 let a 
-const x =25;
+const x =25; 
+  
