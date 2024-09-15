9e987a5937c835da832a092992649a0cc63e4177
vedlearns <Vedankxb@gmail.com>
2024-09-15 20:45:31 +0530

    v12 completed

---
+++ JS/v11/script.js
@@ -0,0 +1,24 @@
+//  console.log(Math.PI);
+//  console.log(Math.sqrt(2.25));
+//  console.log(5**5);
+//  console.log(Math.pow(5,5));
+//  console.log(Math.floor(3.6));
+//  console.log(Math.floor(-6.5));
+//  console.log(Math.ceil(2.5));
+//  console.log(Math.ceil(-2.5));
+//  console.log(Math.round(3.2));
+//  console.log(Math.round(3.49));
+//  console.log(Math.round(-3.5));
+//  console.log(Math.round(-3.59));
+//  console.log(Math.round(Math.random()*100));
+
+// <--Area of a rectangle-->
+// const width=prompt(`please enter rectangle width`);
+// const height=prompt(`please enter rectangle height`);
+// console.log(`Area of the Rectangle is ${+width*+height}`);
+//                      or
+
+const width=+prompt(`please enter height of rectangle`);
+const height=+prompt(`please enter width of rectangle`);
+console.log(`Area of a Rectangle is ${width*height}`);
+document.write(`Area is ${width*height}`);
