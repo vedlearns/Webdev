df42d47808cde2c79db71a9e7d312991b08c29e1
vedlearns <Vedankxb@gmail.com>
2024-09-15 18:57:58 +0530

    v10 completed

---
+++ JS/v10/script.js
@@ -0,0 +1,41 @@
+// <--- REPLACE---->
+// let message ="hi,Ved Ved";
+// message=message.replace("ed","Hello");
+// console.log(message);
+
+//  <-----how to add two strings----> 
+// let message="hi";
+// let FinalMessage=" Ved";
+// console.log(message+FinalMessage);
+// console.log("hi"+" Vedu");
+
+// <----no. to string----->
+// let v=8;
+// v is number
+// v=9+' ';
+// console.log(v);
+// v is string 
+
+// <--String Literal--->
+// const LastFourDigits=" 7865"
+// const b="this is so cool"
+// console.log("hi this is my accnt ".concat(","+LastFourDigits));
+// const templateString=`this is my accnt $${" this is a string"}`;
+// const templateString2=`this is my accnt ${b}`;
+// console.log(templateString);
+// console.log(templateString2);
+// console.log(`my accnt is ${LastFourDigits.padStart(8,"*")}`);
+
+
+// <-- concat-->
+// let message ="Hi";
+// let FinalMessage=" Ved";
+// console.log(message.concat(FinalMessage));
+// console.log(message.concat(","+FinalMessage+".")); 
+
+
+// <----Let & Var Diff---->
+// {
+//     var a=0;
+// }
+//  console.log(a);
