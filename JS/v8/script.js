fd080883fbeed6897c00e3f578ab1f956c444f63
vedlearns <Vedankxb@gmail.com>
2024-09-16 18:51:08 +0530

    v13 complete

--- JS/v8/script.js
+++ JS/v8/script.js
@@ -6,4 +6,10 @@ let isHappy = true
 let userIntro ="hello i am "+ username + "singh "
 let a 
 const x =25; 
-  
+
+
+// <---truthy and falsy value---->
+// const andResult =0&&2;  answer 0
+// const orResult=0||2; answer 2
+// const andResult2='hello'||'' answer hello
+// const andResult2='hello'&&'' answer ''
