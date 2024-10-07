b1d4004c0f4a40dcc5db9eee0f3b3e4b3cc549e4
vedlearns <Vedankxb@gmail.com>
2024-10-08 02:29:03 +0530

    v42 Completed

---
+++ JS/v42/script.js
@@ -0,0 +1,27 @@
+
+const math={                        // OBJECT
+
+    // Functions
+    //old way of declaration
+    add:function(a,b){
+        return a+b;
+    },
+    square:function(a){
+        return a**2;
+    },
+
+    //<new way of declaration>
+    subtract (a,b){
+        return a-b;
+    }
+}
+
+// every Method is a Function but every Function is not a method 
+
+// Jo function ek object ke andr declare ho rakha hai use hum METHOD kehte hai 
+
+
+
+// To access these methods -
+// console-
+//              math.subtract(6,5)
