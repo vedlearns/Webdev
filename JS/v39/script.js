54568cc7fa1488e6de4f2eac9f4e335711b86ca0
vedlearns <Vedankxb@gmail.com>
2024-10-07 23:51:01 +0530

    v40 completed

--- JS/v39/script.js
+++ JS/v39/script.js
@@ -30,3 +30,19 @@ clearTimeout(timer6);                                           // same work as 
 
 setTimeout(function(){console.log("yoink");},0)             // asynchronous code       // MOST common way of using setTimeout (anonymous func)
 console.log("first");                                         //  synchronous code
+
+
+//<---------------------------------------      v40       ----------------------------------->
+// <---ASYNCHRONOUS AND SYNCHRONOUS CODE---->
+//functions like setTimeout in sb ka execution js ke under nhi ata ye aata h webapis ke , browser engine ke under
+// jb setTimeout jaisa function ata code me to vo normal CALL-STACK me bheje jane ke bjaaye vo Jata h CALL-QUEUE , jitne function browser engine se honge vo sb line se hote h , aur end me execute hote 
+// ek hota h EVENT LOOP jo call stack aur call queue ke beech me kaam krta h , uska function hota h check krna ki call queue khali h ki nhi ,agr khali nhi hoti h to use call stacke me dalta jata h turn by turn 
+// ye sb code kehlate h ASYNCHRONOUS CODE(jo seedha call stack me na jaaye , call queue hote hue jaye)
+
+//MDN DEFINITION- Asynchronous programming is a technique that enables your program to start a potentially long-running task and still be able to be responsive to other events while that task runs, rather than having to wait until that task has finished. Once that task has finished, your program is presented with the result.
+//W3SCHOOL - "I will finish later!"
+//           Functions running in parallel with other functions are called asynchronous
+//           A good example is JavaScript setTimeout()
+
+// SYNCHRONOUS CODE -
+// jo code line by line execute hote h JS engine se , ek ke ek baad execute honge. 
