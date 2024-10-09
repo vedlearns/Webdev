ee4ccfef1b420755e87c9bf9f23112d03d2a9ed3
vedlearns <Vedankxb@gmail.com>
2024-10-09 20:32:49 +0530

    v45 completed

--- JS/v44/script.js
+++ JS/v44/script.js
@@ -58,11 +58,38 @@ const personKeys=Object.keys(person);                               //For gettin
  console.log(personEntries);                                            //(4) [Array(2), Array(2), Array(2), Array(2)]      //ek array jiske andr  aur array hai jo value or keys store kar rhe hai  
  for(const e of personEntries)
  {
-    console.log(person[e[1]]);                                      // ye maine kiya hehehe
+    console.log(person[e[0]]);                                      // ye maine kiya hehehe
  }
 
  // AGR check krna ki koi element kisi object me hai ki nhi 
  console.log("firstName" in person);                                //true      // ye dhundne ke liye "in" key word use krte joke for in se totally different hai
 
 
+ 
+
+//<-----------------------         V45          ------------------------------->
+
+console.log("\n","Using For of");
+ for(const fruit of fruits)
+ {
+    console.log(fruit);
+ }
+
+ console.log("\n","Using For each");
+
+ //<-----       FOR EACH LOOP           ------->(forEach ek method hota hai,Array ke liye)
+ fruits.forEach(function(fruit){console.log(fruit);})       // jitne bhi elements hai hmare Array me utni baar ye Anonymous function ko call krega
+ // forEach (method/function) is a eg of higher ordered function ,and the anonymous/anonymous function is a callback function , 
+
+ console.log("\n","using Arrow function ");
+
+ //<-----    using Arrow Function       ------>
+fruits.forEach((Fruit)=>{console.log(Fruit);});
+
+//named function bhi pass kr skte h anonymous ki jagh pe 
+function abc(eL){
+    console.log("yooo");
+}
+
+fruits.forEach(abc);                    // (4) yooo
 
