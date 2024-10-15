0aa92d0c7bbb7594b7f1479456de75595fccc795
vedlearns <Vedankxb@gmail.com>
2024-10-15 22:22:41 +0530

    v46 0.5 completed

---
+++ JS/v46/script.js
@@ -0,0 +1,41 @@
+const months=["january","february","march","april","may"];
+// months.forEach((month)=>{console.log(month);})
+months.map((month)=>{console.log(month);})
+
+const capitalMonths=months.map((month)=>{console.log(month);
+    return month.toUpperCase()
+})
+console.log("\n");
+console.log(capitalMonths);                                    //(5) ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY']
+
+//direct printing without storing in a variable
+console.log(months.map((month)=>{console.log(month);
+    return month.toUpperCase()
+}));                                                        //(5) ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY']
+
+
+// we can also print the index along with array values
+const Months=months.map((month,i,arr)=>{console.log(month,i+1);              //january 1 february 2 ...
+    // console.log(arr);                                          //(5) ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY']              // ye har baar array print krega 
+    return month.toUpperCase();                             // ek array return krega jisme upper case me honge months
+})                    
+
+console.log(Months);                                            // (5) ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY']
+console.log('\n');
+
+
+//<---------     FILTER       ----------->
+const filteredMonths=months.filter((month,index,arr)=>{
+    console.log(index+1,month);                                     // 1 'january' 2 'february'....
+    return month.length<=5;                                         //(TRUTHY ya FALSY values check krta fir value return krta hai ".filter()" )  ye ek condition ke hisaab se data return krta (filtering array ) jitna array ke elements  ki length <= 5 hongi sb data jayega store hone "filteredMonths" me , jin elements pe conditions false hongi vo sb skip hojaenge 
+})
+console.log(filteredMonths);                                        //(3) ['march', 'april', 'may']     // kyunki conditions ke hisaab se yhi values aengi
+
+const filteredMonths2=months.filter((month,index)=>{
+    return month.includes("m"||"M");
+    // return index>2;                                              //(2) ['april', 'may']
+})
+console.log(filteredMonths2);                                       // (2) ['march', 'may']
+
+
+const students=[{},]
