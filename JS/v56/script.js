3b3ea7eba0e46dcf5252bf835c1e33a1825147cf
vedlearns <Vedankxb@gmail.com>
2024-11-05 23:59:03 +0530

    v56 completed

---
+++ JS/v56/script.js
@@ -0,0 +1,9 @@
+// const para=document.querySelector("p");
+// para.innerHTML="<h1>Hiii<h1>";
+
+const para=document.querySelector("p");
+
+para.textContent="<h1>Hiii<h1>"     // <h1>Hiii<h1>
+
+// INNER TEXT - me jo text rhta h elements jitna hm dikhana chahte h , properties use krke jaise "visibility"/"opacity" , jitna hm screen pe dikhana chahte , jo hmne text me diya h , sirf wahi dikhega ya uspe act hoga 
+// INNER content - me vo sb ayega jo hmne originally likha , bhele hm use screen pe show kr rhe ya nhi , aur exact ditto same vaisa hi rhta h , including all the spaces and changing line "\n" use krke , bhel property se hide kiya ho , jo jo originally context likha h vo pura ayega aur uspe act hoga 
