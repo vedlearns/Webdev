3324338c2c19b7eb862f5987ec38bfa2eabc744b
vedlearns <Vedankxb@gmail.com>
2025-01-05 23:54:58 +0530

    v89 0.5 completed

---
+++ JS/v88/country.js
@@ -0,0 +1,45 @@
+
+const countryName = new URLSearchParams(window.location.search).get("name");
+const flagImg = document.querySelector(".country-details img");
+const countryNameH1=document.querySelector(".country-details h1")
+const nativeName=document.querySelector(".nativeName")
+const population=document.querySelector(".population")
+const region=document.querySelector(".region")
+const subRegion=document.querySelector(".subRegion")
+const capital=document.querySelector(".capital")
+const domain=document.querySelector(".domain")
+const currencies=document.querySelector(".currencies")
+const language=document.querySelector(".language")
+const borderPara=document.querySelector(".border-countries")
+
+fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
+.then(res=>res.json())
+.then(([country])=>{
+    console.log(country);
+    console.log(country.name.nativeName);
+    flagImg.src=country.flags.svg
+    countryNameH1.innerText=country.name.common
+    nativeName.innerHTML=`<b>Native Name: </b>&nbsp;${country.name.nativeName? Object.values(country.name.nativeName)[0].common : country.name.common}`
+    population.innerHTML=`<b>Population: </b>&nbsp;${country.population}`
+    region.innerHTML=`<b>Region: </b>&nbsp;${country.region}`
+    subRegion.innerHTML=`<b>Sub Region: </b>&nbsp;${country.subregion ?? country.region}`
+    capital.innerHTML=`<b>Capital: </b>&nbsp;${country.capital ? country.capital.join(",") :" No Official Capital"}`
+    domain.innerHTML=`<b>Top Level Domain: </b>&nbsp;${country.tld.join(' , ')}` 
+    currencies.innerHTML=`<b>Currencies: </b>&nbsp;${country.currencies?  Object.values(country.currencies).map((currency)=>currency.name).join(", ") :"no Official Currency"}`
+    language.innerHTML=`<b>Languages: </b>&nbsp;${country.languages? Object.values(country.languages).join(" , "):"No Official Language"}`
+    if(country.borders)
+    {   
+        country.borders.forEach((border) => {
+            console.log(border);
+            fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+            .then(res=>res.json())
+            .then(([borderCountryData])=>{
+                console.log(borderCountryData);
+                const borderCountry=document.createElement("a")
+                borderCountry.href=`country.html?name=${borderCountryData.name.common}`
+                borderCountry.innerText=borderCountryData.name.common
+                borderPara.append(borderCountry)
+            })
+        });
+    }
+})
--- JS/v88/script.js
+++ JS/v88/script.js
@@ -1,28 +1,64 @@
-
 const cardContainer = document.querySelector(".countries-container");
-fetch("https://restcountries.com/v3.1/all")
+let allCountriesData
+ fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
-    for (let i = 0; i < data.length; i++) {
-    const countryCard = document.createElement("a");
-    countryCard.classList.add("country-card");
-    countryCard.innerHTML =  `<img class="flag-img" src="https://flagcdn.com/ch.svg" alt="">
-    <div class="card-content">
-        <h3 class="Name">Switzerland</h3>
-        <p class="Population"><b>Population:&nbsp;</b>81,770,900</p>
-        <p class="Region"><b>Region:&nbsp;</b>Europe</p>
-        <p class="Capital"><b>Capital:&nbsp;</b>Berlin</p>    
-    </div>`;
-    countryCard.querySelector(".Name").innerText=data[i].name.common
-    countryCard.querySelector(".Population").innerHTML=`<b>Population:&nbsp;</b>${data[i].population.toLocaleString('en-IN')}`
-    countryCard.querySelector(".Region").innerHTML=`<b>Region: &nbsp;</b>${data[i].region}`
-    countryCard.querySelector(".Capital").innerHTML=`<b>Capital: &nbsp;</b>${data[i].capital}`
-    countryCard.querySelector(".flag-img").src=`${data[i].flags.svg}`
-    cardContainer.append(countryCard) 
-    }
-
+    renderCountries(data)
+    allCountriesData=data
   });
+const searchInput=document.querySelector(".search-container input")
+console.log(searchInput);
 
-  fetch("https://restcountries.com/v3.1/name/switzerland")
-  .then(res=>res.json())
-  .then(data=>console.log(data[0].name))
+
+const filterByRegion = document.querySelector(".filter");
+filterByRegion.addEventListener("change", () => {
+if(filterByRegion.value=="No Filter"){
+  location.reload();
+}
+else{
+  console.log(filterByRegion.value);
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      renderCountries(data);
+    });
+}
+
+});
+
+function renderCountries(data) {
+  cardContainer.innerHTML = ``;
+  for (let i = 0; i < data.length; i++) {
+    const countryCard = document.createElement("a");
+    countryCard.href = `country.html?name=${data[i].name.common}`;
+    countryCard.classList.add("country-card");
+    countryCard.innerHTML = `<img class="flag-img" src="https://flagcdn.com/ch.svg" alt="">
+      <div class="card-content">
+          <h3 class="Name">Switzerland</h3>
+          <p class="Population"><b>Population:&nbsp;</b>81,770,900</p>
+          <p class="Region"><b>Region:&nbsp;</b>Europe</p>
+          <p class="Capital"><b>Capital:&nbsp;</b>Berlin</p>    
+      </div>`;
+    countryCard.querySelector(".Name").innerText = data[i].name.common;
+    countryCard.querySelector(
+      ".Population"
+    ).innerHTML = `<b>Population:&nbsp;</b>${data[i].population.toLocaleString(
+      "en-IN"
+    )}`;
+    countryCard.querySelector(
+      ".Region"
+    ).innerHTML = `<b>Region: &nbsp;</b>${data[i].region}`;
+    countryCard.querySelector(".Capital").innerHTML = `<b>Capital: &nbsp;</b>${
+      data[i].capital ? data[i].capital.join(",") : " No Official Capital"
+    }`;
+    countryCard.querySelector(".flag-img").src = `${data[i].flags.svg}`;
+    countryCard.querySelector(".flag-img").alt = `${data[i].flags.svg}`;
+    cardContainer.append(countryCard);
+  }
+}
+
+searchInput.addEventListener("input",(e)=>{
+console.log(e.target.value);
+const filterCountries=allCountriesData.filter((country)=> country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+renderCountries(filterCountries)
+})
